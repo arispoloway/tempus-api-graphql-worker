08878cda803cf500b40899a9823139c294ff0dbb
Aris Poloway <agpoloway@gmail.com>
2023-03-17 22:54:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## tempus-api-graphql-worker
+
+A [Cloudflare Worker](https://workers.cloudflare.com/) deployed to [tempus.nolem.me](https://tempus.nolem.me) exposing a GraphQL interface for the [tempus.xyz](https://tempus.xyz).
+
+Loading the site in a browser will show a playground allowing you to experiment with the API, and the site accepts `POST` requests to `https://tempus.nolem.me/` with the standard GraphQL parameters as `application/json`:
+
+```
+POST https://tempus.nolem.me/
+{
+  "query": "{ map { ... } }",
+  "variables": { "myVariable": "..." }
+}
+```
+
+This worker is mostly just a wrapper around the [tempus-api-graphql](https://github.com/arispoloway/tempus-api-graphql) package.
---
+++ index.js
@@ -0,0 +1,55 @@
+import { Router } from 'itty-router';
+import { newCachedTempusFetcher, newTempusSchema } from 'tempus-api-graphql';
+import { graphql } from 'graphql';
+import playground from './playground';
+
+// Create a new router
+const router = Router();
+
+const BASE_URL = "https://tempus.xyz/api/";
+
+
+async function fetchResponseByUrl(url) {
+    let res = await fetch(`${BASE_URL}${url}`);
+    res = await res.json();
+    return res.error ? null : res;
+}
+
+const schema = newTempusSchema(newCachedTempusFetcher(fetchResponseByUrl));
+
+function graphQLFetcher(graphQLParams) {
+  return graphql(
+    schema,
+    graphQLParams.query,
+    null,
+    null,
+    graphQLParams.variables
+  );
+}
+
+router.post('/', async (request) => {
+    let body = await request.json();
+    let result = await graphQLFetcher({ query: body.query, variables: body.variables });
+    const resultString = JSON.stringify(result, null, 2);
+    return new Response(resultString, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+});
+
+const graphQLOptions = {
+  baseEndpoint: '/',
+  playgroundEndpoint: '/',
+  forwardUnmatchedRequestsToOrigin: false,
+  debug: false,
+  cors: true,
+}
+
+router.get('/', (request) => playground(request, graphQLOptions));
+
+router.all('*', () => new Response('404 Not Found', { status: 404 }));
+
+export default {
+	fetch: router.handle,
+};
